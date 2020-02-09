16e872f5710e82605ee1b3c6710e2445c5e465a8
asuraiv <asuraiv7@gmail.com>
2020-02-09 23:20:44 +0900

    이진 탐색 (using for each) 작성

--- src/main/java/com/asuraiv/algorithm/search/BinarySearch.java
+++ src/main/java/com/asuraiv/algorithm/search/BinarySearch.java
@@ -31,12 +31,44 @@ public class BinarySearch {
 		return binarySearch(S, low, high, x);
 	}
 
+	private int searchUsingForEach(int[] S, int x) {
+
+		int low = 1;
+		int high = S.length;
+		int middle = -1;
+
+		while(low <= high) {
+
+			middle = (low + high) / 2;
+
+			if(x == S[middle]) {
+				break;
+			} else if(x < S[middle]) {
+				high = middle - 1;
+			} else {
+				low = middle + 1;
+			}
+		}
+
+		return middle;
+	}
+
 	@Test
-	public void testSequentialSearch() {
+	public void testBinarySearch() {
 
 		int[] S = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 		int index = search(S, 8);
+
+		assertThat(index, is(7));
+	}
+
+	@Test
+	public void testBinarySearchUsingForEach() {
+
+		int[] S = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+		int index = searchUsingForEach(S, 8);
 
 		assertThat(index, is(7));
 	}
