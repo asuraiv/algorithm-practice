819e96b9952b7ada894f08eed39c1c847b6cba88
asuraiv <asuraiv7@gmail.com>
2020-02-21 16:37:52 +0900

    꼬리재귀 Fibonacci 추가

--- src/main/java/com/asuraiv/algorithm/fibonacci/RecursiveFibonacci.java
+++ src/main/java/com/asuraiv/algorithm/fibonacci/RecursiveFibonacci.java
@@ -10,17 +10,30 @@ public class RecursiveFibonacci {
 
 	int fib(int n) {
 
-		if(n <= 1) {
+		if (n <= 1) {
 			return n;
 		}
 
 		return fib(n - 1) + fib(n - 2);
 	}
 
+	int fib(int n, int a, int b) {
+
+		if (n == 0) {
+			return a;
+		}
+
+		if (n == 1) {
+			return b;
+		}
+
+		return fib(n - 1, b, a + b);
+	}
+
 	@Test
 	public void testFib() {
 
-		int n = fib(6);
+		int n = fib(6, 0, 1);
 
 		assertThat(n, is(8));
 	}
