2cd2fb795addf5806b1df2193c331b4a59eb0897
asuraiv <asuraiv7@gmail.com>
2020-04-01 23:49:54 +0900

    topcoder 문제풀이 코드 추가

---
+++ src/main/java/com/asuraiv/algorithm/topcoder/basic/InterestingDigits.java
@@ -0,0 +1,46 @@
+package com.asuraiv.algorithm.topcoder.basic;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/*
+	SRM150, Div 2, Level 2
+ */
+public class InterestingDigits {
+
+	public int[] digits(int base) {
+
+		List<Integer> digits = new ArrayList<>();
+
+		for(int i = 2; i < base; i++) {
+
+			if((base - 1) % i == 0) {
+				digits.add(i);
+			}
+		}
+
+		int[] results = new int[digits.size()];
+
+		for (int i = 0; i < digits.size(); i++) {
+			results[i] = digits.get(i);
+		}
+
+		return results;
+	}
+
+	@Test
+	public void test() {
+
+		int base = 10;
+
+		int[] results = digits(base);
+
+		assertThat(results[0], is(3));
+		assertThat(results[1], is(9));
+	}
+}
